8f2e08e9116148e930e635b01d0ca3dff8ddefcf
chicori3 <chicori3@naver.com>
2020-12-04 00:30:34 +0900

    Loading Screen

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>보자 보자 어디 보자</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#fdf6aa",
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 20,
+  },
+});
