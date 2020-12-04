37043262e011d9edb3ba2b0336b12ee1ac9ad9a7
chicori3 <chicori3@naver.com>
2020-12-04 23:28:50 +0900

    Get Weather

--- App.js
+++ App.js
@@ -2,16 +2,26 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import axios from "axios";
+
+const API_KEY = "b66526266bbcabf977854dfab045fcbf";
 
 // View = div, Text = span
 export default class extends React.Component {
   state = { isLoading: true };
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const {
         coords: { latitude, longitude },
-      } = await Location.getCurrentPositionAsync();
+      } = await Location.getCurrentPositionAsync(); // 현재 위치를 찾는 메서드
+      this.getWeather(latitude, longitude);
       this.setState({ isLoading: false });
     } catch (error) {}
   };
