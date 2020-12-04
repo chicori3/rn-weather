e6475c610059e2ca0c8184035f5ae1cd53ddb8f4
chicori3 <chicori3@naver.com>
2020-12-04 22:56:16 +0900

    Get Location

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import Loading from "./Loading";
+import * as Location from "expo-location";
 
 // View = div, Text = span
-export default function App() {
-  return <Loading />;
+export default class extends React.Component {
+  getLocation = async () => {
+    const location = await Location.getCurrentPositionAsync();
+  };
+  componentDidMount() {}
+  render() {
+    return <Loading />;
+  }
 }
